cc34d169f3d53a69c2bbbf27ef1ccb60ba5fd36b
Joelen Thomaz <150744092+joelen-thomaz-g@users.noreply.github.com>
2023-11-14 19:08:12 -0300

    Add files via upload

---
+++ aula01.js
@@ -0,0 +1,76 @@
+//var nomeAluno = "João"
+
+/* let saudacao = "Bom dia!"
+
+console.log(saudacao + nomeAluno)
+-----------------------------------------------------*/
+
+//var nome = "Caixa d'água"
+/* const resposta = `Eu trabalho próximo ${nome}`
+console.log(resposta); 
+-----------------------------------------------------*/
+
+//let eDia = true
+/* console.log(typeof eDia) 
+-----------------------------------------------------*/
+
+//const car = {
+    /* cor: "Branco",
+    qtdPortas: 5,
+    categoria: "Hatch",
+    nome: "Fiat Uno"
+}
+
+console.log(car)
+-----------------------------------------------------*/
+
+/* const notas = [8, 7.5, 9.3, 6.5, "Joelen"]
+console.log(notas[4]); 
+-----------------------------------------------------*/
+
+/*  const distanciaEmKm = 100;
+const precoCombustivel = 5.89;
+const kmPorLitros = 14;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+
+const valorGasto = litrosConsumidos + precoCombustivel;
+
+console.log(`O total gasto nesta viagem será de R$ ${valorGasto.toFixed(2)}`); 
+
+console.log("O total gasto nesta viagem será de R$" + valorGasto.toFixed(2));
+----------------------------------------------------------------------------------------*/
+
+/* const numero =17;
+const ehNumeroPar = (numero % 2) === 0;
+
+if(ehNumeroPar) {
+    console.log("O número é par");
+} else {
+    console.log("O número é ímpar");
+}
+----------------------------------------------------------------------------------------*/
+
+/* const distanciaEmKm = 100;
+const precoGasolina = 5.89;
+const precoEtanol = 3.89;
+const kmPorLitros = 14;
+const tipoCombustivel = 'gasolina';
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+
+if(tipoCombustivel ==='etanol') {
+    let valorGasto = litrosConsumidos * precoEtanol;
+    console.log(valorGasto.toFixed(2));
+} else {
+    let valorGasto = litrosConsumidos * precoGasolina;
+    console.log(valorGasto.toFixed(2));
+}   
+-------------------------------------------------------------------------------*/
+
+
+
+
+
+
+
